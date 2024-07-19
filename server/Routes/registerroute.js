6192d4859022a18b142aba3e5a8a5db122e111c9
AliOding12 <aliz17043@gmail.com>
2024-07-19 09:00:00 +0500

    Add error handling to registration routes

--- server/Routes/registerroute.js
+++ server/Routes/registerroute.js
@@ -8,3 +8,4 @@ router.post('/checkUser',checkUser);
 router.get('/verses/indopak/:chapter_number',getqurantext);
 router.get('/v4/recitations/:recitation_id/by_chapter/:chapter_number',getquranaudio);
 module.exports= router;// Add registration routes for Express
+// Add error handling to registration routes
